448439985160d7f23dd4d25615568eb9755acbc6
klose911 <klose911@gmail.com>
2018-01-27 21:40:02 +0800

    backpropagation in convolutional networks

--- neureal-networks/convolution_neural_network/week1/convolution_model.py
+++ neureal-networks/convolution_neural_network/week1/convolution_model.py
@@ -629,56 +629,56 @@ def conv_backward(dZ, cache):
     
     ### START CODE HERE ###
     # Retrieve information from "cache"
-    (A_prev, W, b, hparameters) = None
+    (A_prev, W, b, hparameters) = cache
     
     # Retrieve dimensions from A_prev's shape
-    (m, n_H_prev, n_W_prev, n_C_prev) = None
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
     
     # Retrieve dimensions from W's shape
-    (f, f, n_C_prev, n_C) = None
+    (f, f, n_C_prev, n_C) = W.shape
     
     # Retrieve information from "hparameters"
-    stride = None
-    pad = None
+    stride = hparameters['stride']
+    pad = hparameters['pad']
     
     # Retrieve dimensions from dZ's shape
-    (m, n_H, n_W, n_C) = None
+    (m, n_H, n_W, n_C) = dZ.shape
     
     # Initialize dA_prev, dW, db with the correct shapes
-    dA_prev = None                           
-    dW = None
-    db = None
+    dA_prev = np.zeros((m, n_H_prev, n_W_prev, n_C_prev), dtype=float)                           
+    dW = np.zeros((f, f, n_C_prev, n_C), dtype=float)
+    db = np.zeros((1,1,1,n_C), dtype=float)
 
     # Pad A_prev and dA_prev
-    A_prev_pad = None
-    dA_prev_pad = None
+    A_prev_pad = np.pad(A_prev, ((0,0), (pad,pad), (pad,pad), (0,0)), 'constant')
+    dA_prev_pad = np.pad(dA_prev, ((0,0), (pad,pad), (pad,pad), (0,0)), 'constant')
     
-    for i in range(None):                       # loop over the training examples
+    for i in range(m):                       # loop over the training examples
         
         # select ith training example from A_prev_pad and dA_prev_pad
-        a_prev_pad = None
-        da_prev_pad = None
+        a_prev_pad = A_prev_pad[i]                     
+        da_prev_pad = dA_prev_pad[i]
         
-        for h in range(None):                   # loop over vertical axis of the output volume
-            for w in range(None):               # loop over horizontal axis of the output volume
-                for c in range(None):           # loop over the channels of the output volume
+        for h in range(n_H):                   # loop over vertical axis of the output volume
+            for w in range(n_W):               # loop over horizontal axis of the output volume
+                for c in range(n_C):           # loop over the channels of the output volume
                     
                     # Find the corners of the current "slice"
-                    vert_start = None
-                    vert_end = None
-                    horiz_start = None
-                    horiz_end = None
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
                     
                     # Use the corners to define the slice from a_prev_pad
-                    a_slice = None
+                    a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
 
                     # Update gradients for the window and the filter's parameters using the code formulas given above
-                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += None
-                    dW[:,:,:,c] += None
-                    db[:,:,:,c] += None
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:,:,:,c] * dZ[i, h, w, c]
+                    dW[:,:,:,c] += a_slice * dZ[i, h, w, c]
+                    db[:,:,:,c] += dZ[i, h, w, c]
                     
         # Set the ith training example's dA_prev to the unpaded da_prev_pad (Hint: use X[pad:-pad, pad:-pad, :])
-        dA_prev[i, :, :, :] = None
+        dA_prev[i, :, :, :] = da_prev_pad[pad:-pad, pad:-pad, :]
     ### END CODE HERE ###
     
     # Making sure your output shape is correct
@@ -768,7 +768,7 @@ def create_mask_from_window(x):
     """
     
     ### START CODE HERE ### (≈1 line)
-    mask = None
+    mask = (x == np.max(x))
     ### END CODE HERE ###
     
     return mask
@@ -845,13 +845,13 @@ def distribute_value(dz, shape):
     
     ### START CODE HERE ###
     # Retrieve dimensions from shape (≈1 line)
-    (n_H, n_W) = None
+    (n_H, n_W) = shape
     
     # Compute the value to distribute on the matrix (≈1 line)
-    average = None
+    average = np.ones(shape) / (n_H * n_W)
     
     # Create a matrix where every entry is the "average" value (≈1 line)
-    a = None
+    a = average * dz
     ### END CODE HERE ###
     
     return a
@@ -902,52 +902,52 @@ def pool_backward(dA, cache, mode = "max"):
     ### START CODE HERE ###
     
     # Retrieve information from cache (≈1 line)
-    (A_prev, hparameters) = None
+    (A_prev, hparameters) = cache
     
     # Retrieve hyperparameters from "hparameters" (≈2 lines)
-    stride = None
-    f = None
+    stride = hparameters['stride']
+    f = hparameters['f']
     
     # Retrieve dimensions from A_prev's shape and dA's shape (≈2 lines)
-    m, n_H_prev, n_W_prev, n_C_prev = None
-    m, n_H, n_W, n_C = None
+    m, n_H_prev, n_W_prev, n_C_prev = A_prev.shape
+    m, n_H, n_W, n_C = dA.shape
     
     # Initialize dA_prev with zeros (≈1 line)
-    dA_prev = None
+    dA_prev = np.zeros((m, n_H_prev, n_W_prev, n_C_prev), dtype=float)
     
-    for i in range(None):                       # loop over the training examples
+    for i in range(m):                       # loop over the training examples
         
         # select training example from A_prev (≈1 line)
-        a_prev = None
+        a_prev = A_prev[i,:,:,:]
         
-        for h in range(None):                   # loop on the vertical axis
-            for w in range(None):               # loop on the horizontal axis
-                for c in range(None):           # loop over the channels (depth)
+        for h in range(n_H):                   # loop on the vertical axis
+            for w in range(n_W):               # loop on the horizontal axis
+                for c in range(n_C):           # loop over the channels (depth)
                     
                     # Find the corners of the current "slice" (≈4 lines)
-                    vert_start = None
-                    vert_end = None
-                    horiz_start = None
-                    horiz_end = None
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
                     
                     # Compute the backward propagation in both modes.
                     if mode == "max":
                         
                         # Use the corners and "c" to define the current slice from a_prev (≈1 line)
-                        a_prev_slice = None
+                        a_prev_slice = a_prev[vert_start:vert_end, horiz_start:horiz_end, c]
                         # Create the mask from a_prev_slice (≈1 line)
-                        mask = None
+                        mask = create_mask_from_window(a_prev_slice)
                         # Set dA_prev to be dA_prev + (the mask multiplied by the correct entry of dA) (≈1 line)
-                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += None
+                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += mask * dA[i, h, w, c] 
                         
                     elif mode == "average":
                         
                         # Get the value a from dA (≈1 line)
-                        da = None
+                        da = dA[i, h, w, c]
                         # Define the shape of the filter as fxf (≈1 line)
-                        shape = None
+                        shape = (f, f) 
                         # Distribute it to get the correct slice of dA_prev. i.e. Add the distributed value of da. (≈1 line)
-                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += None
+                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += distribute_value(da, shape) 
                         
     ### END CODE ###
     
